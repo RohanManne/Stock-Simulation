1ff6a9caa6b436748a9e80a225bdaecb93b8d995
RohanManne <47373086+RohanManne@users.noreply.github.com>
2023-05-21 08:42:07 -0700

    Update positions.py

--- positions.py
+++ positions.py
@@ -1,4 +1,6 @@
 import Stocks
 class Positions:
-    def __init__(self, priceBougt, ):
-        pass
+    def __init__(self, priceBougt, numShares, ticker):
+        self.priceBought = priceBougt
+        self.numShares = numShares
+        self.ticker = ticker
