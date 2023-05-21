b5d7f8cf98a5c33458713ec1ecbf57ad04f4ae4b
RohanManne <47373086+RohanManne@users.noreply.github.com>
2023-05-21 08:27:48 -0700

    Update Stocks.py

--- Stocks.py
+++ Stocks.py
@@ -1,16 +1,24 @@
 class Stock:
     
+    priceHistory = {}
     "priceHistory is a hashSet with Keys: time and date, and values: price at that key"
-    def __init__(self, name, ticker, price, priceHistory, sector, type):
+    "TODO: add sharesBought, shares Sold"
+    def __init__(self, name, ticker, price, sector, type, divs, fees, time):
         self.name = name
         self.ticker = ticker
         self.price = price
-        self.priceHistory = priceHistory
+        priceHistory.update({time: price})
         self.sector = sector
         self.type = type
+        self.divs = divs
+        self.fees = fees
 
     def getInfo(self):
-        return f"{self.name}({self.ticker}) is currently trading at {self.price}"
-    
+        return [self.name][self.ticker][self.price]
+
+    def UpdatePrice(newTime, newPrice):
+        priceHistory.update({newTime:newPrice})
+        price = newPrice
+        time = newTime
     
     
