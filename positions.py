e2f67368d6ae38498cd50538986d18703e08f0b3
o2i6 <arbitrary152@gmail.com>
2023-05-24 18:41:37 -0400

    position change

--- positions.py
+++ positions.py
@@ -1,6 +1,7 @@
 import Stocks
+
 class Positions:
-    def __init__(self, priceBougt, numShares, ticker):
-        self.priceBought = priceBougt
-        self.numShares = numShares
+    def __init__(self, initial_price, num_shares, ticker):
+        self.initial_price = initial_price
+        self.numShares = num_shares
         self.ticker = ticker
